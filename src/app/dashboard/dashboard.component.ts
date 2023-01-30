0da7b2162f5c5d041a9e771ca9f1d6b6116eeceb
quebeen <kejatori@gmail.com>
2023-01-31 02:42:28 +0800

    update UIs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  ngOnInit(): void {
+    let chart = new Chart("dashChart", {
+      type: "bar",
+      data: {
+        labels: ['Metals', 'Plastics', 'Papers'],
+        datasets: [{
+          label: 'Recycled Materials',
+          data: [65, 59, 80],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+          ],
+          borderWidth: 1
+        }]
+      }
+    })
+  }
 }
