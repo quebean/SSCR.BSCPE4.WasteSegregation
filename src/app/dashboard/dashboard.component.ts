ac40371a0686ec55f1e4c88c5ffb1fa15fcdab8b
quebeen <kejatori@gmail.com>
2023-01-31 02:51:51 +0800

    fix bundle

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,34 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js/auto';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  ngOnInit(): void {
-    let chart = new Chart("dashChart", {
-      type: "bar",
-      data: {
-        labels: ['Metals', 'Plastics', 'Papers'],
-        datasets: [{
-          label: 'Recycled Materials',
-          data: [65, 59, 80],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-          ],
-          borderWidth: 1
-        }]
-      }
-    })
-  }
 }
